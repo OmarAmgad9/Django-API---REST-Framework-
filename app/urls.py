69415cef760770d49d481a2ad029e04e373c8d99
Omar Amgad <103358321+OmarAmgad9@users.noreply.github.com>
2024-02-15 11:44:28 +0200

    way to write view

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+router.register('guests', views.ViweSets_guest)
+router.register('movie', views.ViewSets_moive)
+router.register('reservation', views.ViewSets_reservation)
 
 urlpatterns = [
     path('', views.home),
@@ -7,9 +12,30 @@ urlpatterns = [
     path('jsonresponsenomodels', views.no_rest_no_model),
     # 2
     path('jsonresposnsewithmodels', views.no_rest_from_model),
-    # 3 
+    # 3 function base views 
         #3.1
         path('functionBaseViewsList', views.FBV_List),
         # 3.2
-        path('functionBaseViewPk/<int:pk>', views.FBV_pk)
+        path('functionBaseViewPk/<int:pk>', views.FBV_pk),
+    #4 class base views
+        #4.1
+        path('classBaseviewslist', views.CVB_List.as_view()),
+        #4.2
+        path('classBaseviewpk/<int:pk>', views.CBV_pk.as_view()),
+    #5
+        #5.1 mixins
+        path('mixins', views.Mixins_list.as_view()),
+        #5.2 minxins
+        path('mixins_pk', views.Mixins_pk.as_view()),
+    #6 Generics
+        #6.1 
+        path('generics',views.Generics_list.as_view()),
+        #6.2
+        path('generics_pk',views.Generics_pk.as_view()),
+    #7 ViewSets
+        path('viewsets/', include(router.urls)),
+    #8 Find Movie
+    path('findmovie', views.find_movie),
+    #9 New Resevartion
+    path('newresvation', views.new_reservation),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 
@@ -61,19 +62,135 @@ def FBV_List(request):
 
 @api_view(['GET','PUT','DELETE'])
 def FBV_pk(request, pk):
-
-    guest = Guest.objects.get(pk=pk)
+    try:
+        guest = Guest.objects.get(pk=pk)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
     # GET
     if request.method == 'GET':
         print(guest)
         serializer = GuestSerializer(guest)
         return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'PUT':
-        serializer = GuestSerializer(data=request.data)
+        serializer = GuestSerializer(guest,data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
     elif request.method == 'DELETE':
         guest.delete()
-        return Response(status=status.HTTP_202_ACCEPTED)
+        return Response(status=status.HTTP_204_NO_CONTENT)
     return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+
+#4 CBV Class Based Views
+from rest_framework.views import APIView
+#4.1 List == GET and Create == Post
+class CVB_List(APIView):
+    def get(self, request):
+        guests = Guest.objects.all()
+        serializer = GuestSerializer(guests, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = GuestSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
+#4.2 GET With key or put or delete
+from django.http import Http404
+class  CBV_pk(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Guest.objects.get(pk=pk)
+        except Guest.DoesNotExist:
+            raise Http404
+    def get(self, request, pk):
+        guest = self.get_object(pk)
+        serializer = GuestSerializer(guest)
+        return Response(serializer.data)
+    def put(self, request, pk):
+        guest = self.get_object(pk)
+        serializer = GuestSerializer(guest, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, pk):
+        guest = self.get_object(pk)
+        guest.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+#5 Mixins 
+from rest_framework import generics, mixins, viewsets
+
+    #5.1
+class Mixins_list(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+    queryset = Guest.objects.all()
+    serializer_class = GuestSerializer
+    
+    def get(self, request):
+        return self.list(request)
+    def post(self, request):
+        return self.create(request)
+    #5.2
+class Mixins_pk(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Guest.objects.all()
+    serializer_class = GuestSerializer
+    def get(self, request):
+        return self.retrieve(request)
+    def put(self, request):
+        return self.update(request)
+    def delete(self, request):
+        return self.destroy(request)
+#6 Generics
+    #6.1 
+class Generics_list(generics.ListCreateAPIView):
+    queryset = Guest.objects.all()
+    serializer_class = GuestSerializer
+
+    #6.2
+class Generics_pk(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Guest.objects.all()
+    serializer_class = GuestSerializer
+
+#7 ViewSets
+class ViweSets_guest(viewsets.ModelViewSet):
+    queryset = Guest.objects.all()
+    serializer_class = GuestSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['movie']
+
+class ViewSets_moive(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class ViewSets_reservation(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+#8 Find Movie 
+@api_view(['GET'])
+def find_movie(request):
+    movie = Movie.objects.filter(movie=request.data['movie'] )
+    serializer = MovieSerializer(movie, many=True)
+    return Response(serializer.data)
+@api_view(['POST'])
+def new_reservation(request):
+    movie = Movie.objects.get(movie=request.data['movie'])
+    guest = Guest()
+    guest.name = request.data['name']
+    guest.mobile = request.data['moblie']
+    guest.save()
+    reservation =  Reservation()
+    reservation.guest = guest
+    reservation.movie = movie
+    reservation.save()
+    serializer = ReservationSerializer(reservation)
+    return Response(serializer.data,status=status.HTTP_201_CREATED)
+
+#9 Create New Reservation
+
